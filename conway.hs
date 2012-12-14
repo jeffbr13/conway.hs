dc1cc12d060898f581e3e5a7209177b6b122f548
Ben Jeffrey <mail@benjeffrey.net>
2012-12-14 17:10:45 +0000

    Don Sannella transcribed quite a few patterns into boards, which are available in 'sample items' at the bottom of the file.

--- conway.hs
+++ conway.hs
@@ -256,3 +256,81 @@ glider = [newCell 0 0, newCell 1 1, newCell 2 1, newCell 0 2, newCell 1 2]
 lwss = [newCell 0 0, newCell 3 0,
         newCell 4 1, newCell 4 2, newCell 4 3,
         newCell 0 2, newCell 1 3, newCell 2 3, newCell 3 3]
+
+
+
+-- add x and y offsets to a configuration of cells
+offset :: Int -> Int -> [Cell] -> [Cell]
+offset a b cs = map (\ActiveCell {x=x, y=y} -> ActiveCell {x=x+a, y=y+b}) cs
+
+-- stationary blob
+bail = [newCell 1 1, newCell 1 2, newCell 2 1, newCell 2 2]
+
+-- more stationary formations
+beehive = [newCell 2 1, newCell 1 2, newCell 3 2, newCell 1 3, newCell 3 3, newCell 2 4]
+boat = [newCell 2 2, newCell 1 3, newCell 2 4, newCell 3 3, newCell 3 2]
+ship = [newCell 1 4, newCell 2 2, newCell 1 3, newCell 2 4, newCell 3 3, newCell 3 2]
+loaf = [newCell 3 1, newCell 2 2, newCell 1 3, newCell 2 4, newCell 3 4, newCell 4 2, newCell 4 3]
+
+-- alternating vertical and horizontal bar
+myblinker = [newCell 1 1, newCell 1 2, newCell 1 3]
+
+-- Another oscillator
+oscillator = [newCell 1 1, newCell 2 1, newCell 3 1, newCell 2 2, newCell 3 2, newCell 4 2]
+
+-- Both of the following formations develop into oscillators
+dec_oscil =
+    [newCell 1 1, newCell 2 1, newCell 3 1, newCell 4 1, newCell 5 1,
+     newCell 6 1, newCell 7 1, newCell 8 1, newCell 9 1, newCell 10 1]
+
+pulsar =
+    [newCell 2 1, newCell 1 2, newCell 2 2, newCell 3 2, newCell 1 3,
+     newCell 3 3, newCell 1 4, newCell 2 4, newCell 3 4, newCell 2 5]
+
+-- moves horizontally at a rate of two cells in four generations
+spaceship =
+    [newCell 1 2, newCell 1 4, newCell 2 1, newCell 3 1, newCell 4 1,
+     newCell 4 4, newCell 5 1, newCell 5 2, newCell 5 3]
+    ++ offset 6 6 bail
+
+-- stationary diagonal "pole" with moving "stripes"
+barberpole =
+    [newCell 1 13, newCell 1 12, newCell 2 13, newCell 3 12, newCell 3 10,
+     newCell 5 10, newCell 5 8, newCell 7 8, newCell 7 6, newCell 9 6,
+     newCell 9 4, newCell 11 4, newCell 11 2, newCell 12 1, newCell 13 2,
+     newCell 13 1]
+    ++ offset 13 13 bail
+
+-- moves horizontally,  leaving a trail of "smoke"
+puffertrain =
+    [newCell 1 16, newCell 2 15, newCell 3 15, newCell 4 18, newCell 4 15,
+     newCell 5 17, newCell 5 16, newCell 5 15, newCell 1 11, newCell 2 10,
+     newCell 2 7, newCell 3 10, newCell 3 9, newCell 3 8, newCell 1 2,
+     newCell 2 1, newCell 3 1, newCell 4 4, newCell 4 1, newCell 5 3,
+     newCell 5 2, newCell 5 1]
+
+-- starts small and grows
+rpentomino = [newCell 2 1, newCell 1 2, newCell 2 2, newCell 2 3, newCell 3 3]
+
+-- shoots a stream of gliders
+glidergun =
+    [newCell 1 5, newCell 1 4, newCell 2 5, newCell 2 4, newCell 12 6,
+     newCell 12 5, newCell 12 4, newCell 13 7, newCell 13 3, newCell 14 8,
+     newCell 14 2, newCell 15 7, newCell 15 3, newCell 16 6, newCell 16 5,
+     newCell 16 4, newCell 17 6, newCell 17 5, newCell 17 4, newCell 22 4,
+     newCell 22 3, newCell 22 2, newCell 23 9, newCell 23 5, newCell 23 4,
+     newCell 23 2, newCell 23 1, newCell 24 11, newCell 24 10, newCell 24 5,
+     newCell 24 4, newCell 24 2, newCell 24 1, newCell 25 10, newCell 25 9,
+     newCell 25 5, newCell 25 4, newCell 25 3, newCell 25 2, newCell 25 1,
+     newCell 26 6, newCell 26 5, newCell 26 1, newCell 30 5, newCell 30 4,
+     newCell 34 3, newCell 34 2, newCell 35 3, newCell 35 2]
+
+-- Moves left and produces a beehive, then moves right and produces
+-- another beehive, then moves left and crashes into the first beehive
+queenbeeshuttle =
+    [newCell 1 1, newCell 2 1, newCell 3 2, newCell 4 3, newCell 4 4,
+     newCell 4 5, newCell 3 6, newCell 2 7, newCell 1 7]
+-- ... but we can get rid of a beehive using a bail
+beehivebail = beehive ++ offset 0 5 bail
+-- ... so we just put a bail on either side of the queenbeeshuttle
+qbsbail = offset 0 2 bail ++ offset 8 0 (queenbeeshuttle ++ offset 12 2 bail)
